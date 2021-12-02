f419327ba7f49a81c315b2a6cf7903e1d63479a6
Nikita L <starfall1011@gmail.com>
2021-12-02 14:30:55 +0300

    config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -34,7 +34,7 @@ const config = {
         exclude: /node_modules/,
       },
       {
-        test: /\.css$/,
+        test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
       // Add your rules for custom modules here
