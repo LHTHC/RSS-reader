c2cff573b0cdb8fcb9ee423a644024218509079a
Nikita L <starfall1011@gmail.com>
2021-12-08 19:13:59 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -14,18 +14,9 @@ module.exports = {
           },
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
       {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
-      },
-      {
-        test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-        use: 'url-loader?limit=10000',
-      },
-      {
-        test: /\.(ttf|eot|svg)(\?[\s\S]+)?$/,
-        use: 'file-loader',
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
