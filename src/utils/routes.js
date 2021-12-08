41574b48563a47c1fc8b3e0df456cffa26ca4249
Nikita L <starfall1011@gmail.com>
2021-12-08 16:19:11 +0300

    fix proxifier

--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,6 +1,3 @@
 export default (url) => {
-  const proxyUrl = new URL('https://hexlet-allorigins.herokuapp.com/get');
-  proxyUrl.searchParams.set('disableCache', true);
-  proxyUrl.searchParams.set('url', url);
-  return proxyUrl.toString();
+  return `https://hexlet-allorigins.herokuapp.com/get?disableCache=true&url=${url}`;
 };
