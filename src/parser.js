dd2ab85adb1ef55b9c992ac4488259eb2f08f033
Nikita L <starfall1011@gmail.com>
2021-12-09 18:41:31 +0300

    add error throw

--- src/parser.js
+++ src/parser.js
@@ -1,6 +1,10 @@
 export default (data) => {
   const parser = new DOMParser();
   const doc = parser.parseFromString(data, 'application/xml');
+  const parseError = doc.querySelector('parsererror');
+  if (parseError) {
+    throw new Error('parsingError');
+  }
   const title = doc.querySelector('channel title').textContent;
   const description = doc.querySelector('channel description').textContent;
   const posts = Array.from(doc.querySelectorAll('item')).map((item) => {
