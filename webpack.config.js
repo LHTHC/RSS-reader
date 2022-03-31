10c32e7d8dd23068a80f6b7c47d9b633cf542f5b
Nikita Lutskin <starfall1011@gmail.com>
2022-03-31 09:38:23 +0300

    add module, remove babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
-module.exports = {
+export default {
   mode: process.env.NODE_ENV || 'development',
+  entry: './src/index.js',
   module: {
     rules: [
       {
